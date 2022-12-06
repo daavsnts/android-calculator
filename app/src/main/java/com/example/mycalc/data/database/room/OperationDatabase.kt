154e213308f70cc992705ec3e066d809adc71191
Davi Santos <daavsantosl@gmail.com>
2022-12-06 20:52:40 -0300

    fix: removing room schema warning

--- app/src/main/java/com/example/mycalc/data/database/room/OperationDatabase.kt
+++ app/src/main/java/com/example/mycalc/data/database/room/OperationDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.mycalc.model.Operation
 
-@Database(entities = [Operation::class], version = 1)
+@Database(entities = [Operation::class], version = 1, exportSchema = false)
 abstract class OperationDatabase : RoomDatabase() {
     abstract fun operationDao(): OperationDao
 
